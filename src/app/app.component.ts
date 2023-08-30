754cfd01614b504b29ba4080f940531e603cf826
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-08-30 08:05:29 +0200

    creating Credit calculation with cpature value ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,19 +17,55 @@ import { Component } from '@angular/core';
 
 // ***** Capture Value with ngModel (app.modulede FormsModule-ni import etmeyi unutma) ******
 
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <h1>Value Capture</h1>
+//     <!-- Burada moterize inputun icindekini geri gondermek ucundur, [] ise kodda modulun icinde yazdigimizin inputun icinde gorunmesi ucundur -->
+//     <input type="name" [(ngModel)]="name" />
+//     <button (click)="clickButton()">Enter</button>
+//   `,
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent {
+//   name: string = 'Rizvan';
+//   clickButton() {
+//     console.log(this.name);
+//   }
+// }
+
+// ***** With Capture Value Credit Calculation  ******
+
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Value Capture</h1>
-    <!-- Burada moterize inputun icindekini geri gondermek ucundur, [] ise kodda modulun icinde yazdigimizin inputun icinde gorunmesi ucundur -->
-    <input type="name" [(ngModel)]="name" />
-    <button (click)="clickButton()">Enter</button>
+    <h1>Credit calculation</h1>
+    <div>
+      <label for="">Kredi Tutari:</label>
+      <input type="text" [(ngModel)]="krediTutari" />
+    </div>
+    <div>
+      <label for="">Faiz Tutari:</label>
+      <select [(ngModel)]="faizTutari">
+        <option>1</option>
+        <option>5</option>
+        <option>10</option>
+      </select>
+    </div>
+    <button (click)="hesabla()">Hesabla</button>
+    <hr />
+    <div>
+      <h2>Ayliq ödemem: {{ ayliqOdeme }}</h2>
+    </div>
   `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name: string = 'Rizvan';
-  clickButton() {
-    console.log(this.name);
+  krediTutari: number = 10000;
+  faizTutari: number = 1;
+  ayliqOdeme: number = 0;
+
+  hesabla() {
+    this.ayliqOdeme = this.krediTutari / this.faizTutari;
   }
 }

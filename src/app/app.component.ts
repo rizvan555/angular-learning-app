9a6d258cbb359732a7b5e88bc5bfa98c0e7fe253
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-08-30 07:38:00 +0200

    capture value with ngModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 
+// ***********  Capture Value with JS *************
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <h1>Value Capture</h1>
+//     <input type="text" (keyup)="getValue($event)" />
+//   `,
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent {
+//   getValue(event: any) {
+//     console.log(event.target.value);
+//   }
+// }
+
+// ***** Capture Value with ngModel (app.modulede FormsModule-ni import etmeyi unutma) ******
+
 @Component({
   selector: 'app-root',
   template: `
     <h1>Value Capture</h1>
-    <input type="text" (keyup)="getValue($event)" />
+    <!-- Burada moterize inputun icindekini geri gondermek ucundur, [] ise kodda modulun icinde yazdigimizin inputun icinde gorunmesi ucundur -->
+    <input type="name" [(ngModel)]="name" />
+    <button (click)="clickButton()">Enter</button>
   `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  getValue(event: any) {
-    console.log(event.target.value);
+  name: string = 'Rizvan';
+  clickButton() {
+    console.log(this.name);
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TestComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
